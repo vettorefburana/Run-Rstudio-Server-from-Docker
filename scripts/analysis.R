c73e8e3b82300b9b22944b0490bfc1d31fdc5efd
vettorefburana <74903161+vettorefburana@users.noreply.github.com>
2021-12-12 15:21:12 +0100

    Add files via upload

---
+++ scripts/analysis.R
@@ -0,0 +1,28 @@
+
+# add lib path
+.libPaths(new = "/packages")
+
+library(ggplot2)
+library(randomForest)
+library(dplyr)
+
+# read csv file
+redwine = read.csv("/home/rstudio/data/winequality-red.csv")
+
+# binary variable for wine quality
+redwine$good_wine<-ifelse(redwine$quality>6,1,0)
+
+# random forest model
+wine_rf <- randomForest(factor(good_wine)~.-quality, redwine, ntree=150)
+
+imp <- importance(wine_rf)
+
+var_importance <- data.frame(Variables = row.names(imp), 
+                            Importance = round(imp[ ,'MeanDecreaseGini'],2))
+
+# rank variable importance
+rank_importance <- var_importance %>%
+  mutate(rank = paste0('#',dense_rank(desc(imp))))
+
+# save ouput as csv
+write.csv(rank_importance, file = "/home/rstudio/data/output.csv")
