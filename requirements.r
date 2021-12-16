2512a0004c995ffe8565af943357b19e39c747be
vettorefburana <74903161+vettorefburana@users.noreply.github.com>
2021-12-16 14:27:09 +0100

    Add files via upload

---
+++ requirements.r
@@ -0,0 +1,12 @@
+
+
+install.packages("MASS")
+install.packages("zoo")
+install.packages("xts")
+install.packages("ggplot2")
+install.packages("ggcorrplot")
+install.packages("kableExtra")
+install.packages("forecast")
+install.packages("data.table")
+install.packages("Hmisc")
+install.packages("olsrr")
